9bed4dd826c710f21fb93870908f26776d6a4ffc
wearzdk <hi@wearzdk.me>
2024-05-16 18:07:58 +0800

    feat: add web example

---
+++ examples/web-example/src/app.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'preact/hooks'
+import type { Client } from 'ultraphx-js-sdk'
+
+import './app.css'
+import { ctx } from './ctx'
+
+export function App() {
+  return (
+    <>
+      <ConnectedClients />
+    </>
+  )
+}
+
+function ConnectedClients() {
+  const [clients, setClients] = useState<Client[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    ctx.client.getConnectedClients().then((res) => {
+      if (res.error || !res.data)
+        throw new Error(res.error || 'Invalid data')
+      setClients(res.data)
+      setLoading(false)
+    })
+  }, [])
+
+  return (
+    <div>
+      <h2>已连接客户端</h2>
+      {loading
+        ? (
+          <p>加载中...</p>
+          )
+        : (
+          <ul>
+            {clients.map(client => (
+              <li key={client.id}>{client.name}</li>
+            ))}
+          </ul>
+          )}
+    </div>
+  )
+}
---
+++ examples/web-example/src/ctx.ts
@@ -0,0 +1,7 @@
+import { createSdkClient } from 'ultraphx-js-sdk'
+
+export const ctx = createSdkClient({
+  baseUrl: 'https://core.hk.dev.wearzdk.me/api',
+  token: 'eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkExMjhHQ00ifQ.gQF0faBvUX9t3FjMngE5dOWetO3NcxiFRUFMAfhhd-4uS4jQwgpu9aCy9d7SmOtSdJqv2dNyxf90PI8hzNsNta2D20K8bDgD5hPjQzn2wrOhiA460mb9oZBZB3hJwNkZaZ2DE1lpKlQJa7rv53Bgz4Ilscva7n8mJ3zzvBE2JQM5gclDlZd9mqDaa1cIPv1t5LzrQBpN7aH_eCBLO1G1osuTQWrAhu-z7MHWB27LcWuFD1jGVP9DlPQYlwoRHPNtTo4Av325tpo4Iojh8gFxzsCcbkhz7KSMiGO-IcRPdKKSlDiOx0mLvcU8VhJWGsfZwa27vmKYFZ0_zABoxMAjfA.bsD2TGMTrEZoyA9g.4UQEfFnGXtnT0Gy4B4r91Tq-3_BGFzRYwuLcAYmZRycrqzdMhEu5rqRNh43LPV3UTKz-NaRdfGds8qwgqyoLkbBuEkErbaes3OWlcgwZOFtwEW8Q75iUaJdCPddAU-_hy78A0tWI7GFdvV0D2Zff-a6B0DUnc9yTsyjEICCxGABmBGUI8Hm1VFmFijmjDOJbjqqoPWDslcjQ9DQ.nw6bnKDx_2uGiEMyXEFBqw',
+  debug: true,
+})
