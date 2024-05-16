e007e67b4e740f85a5eab440ab38822d80be5a3f
wearzdk <hi@wearzdk.me>
2024-05-16 16:54:39 +0800

    fix: 修正url

--- src/plugins/client.ts
+++ src/plugins/client.ts
@@ -3,10 +3,10 @@ import type { BaseClient, Client } from '@/types'
 export function clientPlugin(_client: BaseClient) {
   const client = {
     getConnectedClients() {
-      return _client.http.get<Client[]>('/client/connected')
+      return _client.http.get<Client[]>('/auth/client/connected')
     },
     getPendingClients() {
-      return _client.http.get<Client[]>('/client/pending')
+      return _client.http.get<Client[]>('/auth/client/pending')
     },
   }
 
