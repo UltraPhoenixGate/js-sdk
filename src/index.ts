5c094cc11120c5f505e777597f63200ee8caeb16
wearzdk <hi@wearzdk.me>
2024-06-22 21:56:25 +0800

    feat: add system api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { type BaseClientOptions, ClientWithPlugins, createBaseSdkClient } from './sdk'
 import { alertPlugin, authPlugin, cameraPlugin, clientPlugin, dataPlugin } from './plugins'
+import { systemPlugin } from './plugins/system'
 
 export * from './sdk'
 export * from './types'
@@ -12,6 +13,7 @@ export function createSdkClient(opt: BaseClientOptions) {
     .use(clientPlugin)
     .use(authPlugin)
     .use(cameraPlugin)
+    .use(systemPlugin)
     .build()
 
   return client
